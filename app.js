85d42570d335c327c054d73042517f865fb61e62
JEWON-320S\Jewon Choi <jewon@khu.ac.kr>
2018-08-08 15:38:03 +0900

    add: Scheduling updating data, Minor improvement

--- app.js
+++ app.js
@@ -1,45 +1,33 @@
 const http = require('http');
 const fs = require('fs');
-const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+// const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 var express = require('express');
 var app = express();
+const schedule = require('node-schedule');
 const hostname = 'localhost';
 const service_port = 1337;
 var locator_geojson = require('./modules/miselocator_geojson.js');
 var get_miseloc_arr = require('./modules/geojson_getnamearray.js');
 var get_distance_XY = require('./modules/getDistanceFromXY.js');
+var req_mise = require('./modules/req_mise.js');
 var doIDW = require('./modules/IDW.js');
 var keys = require('./_config.js');
 
+// 타이머
+const startTime = new Date(Date.now() + 3000); // 3초 뒤 서비스
+const req_mise_min  = startTime.getMinutes();
+const req_mise_sec = startTime.getSeconds();
+console.log("PM Value will be update every " + req_mise_min + "m " + req_mise_sec + "s");
+
 // 측정소 위치 불러옴
 var mise_geojson = locator_geojson();
-// 조회할 시도명 코드들
-var mise_sido = ["서울", "부산", "대구", "인천", "광주", "대전", "울산", "경기", "강원", "충북", "충남", "전북", "전남", "경북", "경남", "제주", "세종"]
 
-// 시도별로 조회해서 측정소 위치 Geojson에 미세먼지 데이터 삽입
-mise_sido.map(function(sido){
-  var xhr = new XMLHttpRequest();
-  var mise_url = "http://openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInqireSvc/getCtprvnRltmMesureDnsty?numOfRows=100&ver=1.3&_returnType=JSON"
-  mise_url += "&sidoName=" + encodeURIComponent(sido);
-  mise_url += "&ServiceKey=" + keys.mise;
-  xhr.open("GET", mise_url, true);
-  xhr.send();
-  xhr.onreadystatechange = function() {
-  	if (xhr.readyState == 4 && xhr.status == 200) {
-      var temp = JSON.parse(xhr.responseText);
-      temp.list.map((obj) => {
-        // 이름 같은 인덱스 찾아서 geojson의 속성에 pm25, pm10값 삽입
-        let i = mise_geojson.features.findIndex((loc) => loc.properties.stationName == obj.stationName)
-        mise_geojson.features[i].properties.pm25value = obj.pm25Value;
-        mise_geojson.features[i].properties.pm10value = obj.pm10Value;
-        mise_geojson.features[i].properties.pm25grade = obj.pm25Grade;
-        mise_geojson.features[i].properties.pm10grade = obj.pm10Grade;
-      })
-    }
-    else {
-      console.log(xhr.status + ": req failed" + sido)
-    }
-  }
+// 1시간마다 반복작업 (node-schedule)
+var req_times = 0;
+var j = schedule.scheduleJob(req_mise_sec + ' ' + req_mise_min + ' * * * *', function(){
+  // 각 측정소에 미세먼지 값 요청해 추가
+  req_mise(mise_geojson, keys.mise);
+  console.log("---Reqest Airkorea API " + (++req_times) + " times---" )
 });
 
 // 메인
@@ -76,13 +64,13 @@ app.get('/api/mise/latlng/:lat/:lng', (req, res) => {
   }
   var distance_value = [];
 
-// 반경 10km 이내 Station 찾기
-mise_geojson.features.forEach(s => {
-  let distance = get_distance_XY(reqlat, reqlng, s.geometry.coordinates[1], s.geometry.coordinates[0]);
-  if (distance < 10 && s.properties.pm10value !== "-" && s.properties.pm10value ) {
-    distance_value.push({"distance" : distance, "value" : s.properties.pm10value})
-  };
-});
+// 반경 10km 이내 Station 찾기 (getDistanceFromXY 모듈)
+  mise_geojson.features.forEach(s => {
+    let distance = get_distance_XY(reqlat, reqlng, s.geometry.coordinates[1], s.geometry.coordinates[0]);
+    if (distance < 10 && s.properties.pm10value !== "-" && s.properties.pm10value ) {
+      distance_value.push({"distance" : distance, "value" : s.properties.pm10value})
+    };
+  });
 
   // 찾은 station들의 먼지값 IDW계산
   if (!distance_value[0]) {
---
+++ modules/req_mise.js
@@ -0,0 +1,33 @@
+// 측정소 geojson 받아서 미세먼지 데이터 요청해 추가해주는 모듈
+const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+
+module.exports = function(_mise_geojson, _key){
+  const mise_sido = ["서울", "부산", "대구", "인천", "광주", "대전", "울산", "경기", "강원", "충북", "충남", "전북", "전남", "경북", "경남", "제주", "세종"]
+
+  // 시도별로 조회해서 측정소 위치 Geojson에 미세먼지 데이터 삽입
+  mise_sido.map(function(sido){
+    var xhr = new XMLHttpRequest();
+    var mise_url = "http://openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInqireSvc/getCtprvnRltmMesureDnsty?numOfRows=100&ver=1.3&_returnType=JSON"
+    mise_url += "&sidoName=" + encodeURIComponent(sido);
+    mise_url += "&ServiceKey=" + _key;
+    xhr.open("GET", mise_url, true);
+    xhr.send();
+    xhr.onreadystatechange = function() {
+    	if (xhr.readyState == 4 && xhr.status == 200) {
+        var temp = JSON.parse(xhr.responseText);
+        temp.list.map((obj) => {
+          // 이름 같은 인덱스 찾아서 geojson의 속성에 pm25, pm10값 삽입
+          let i = _mise_geojson.features.findIndex((loc) => loc.properties.stationName == obj.stationName)
+          _mise_geojson.features[i].properties.pm25value = obj.pm25Value;
+          _mise_geojson.features[i].properties.pm10value = obj.pm10Value;
+          _mise_geojson.features[i].properties.pm25grade = obj.pm25Grade;
+          _mise_geojson.features[i].properties.pm10grade = obj.pm10Grade;
+        })
+      }
+      else {
+        console.log(xhr.status + ": req failed" + sido)
+      }
+    }
+  });
+  return _mise_geojson;
+}
